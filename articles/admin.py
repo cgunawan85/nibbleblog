f47d0ffd1e8d5475a22d9772182d2cd82632b76e
Chris Gunawan <chris@nibble.online>
2018-10-26 07:14:40 +0700

    ignore venv

--- articles/admin.py
+++ articles/admin.py
@@ -3,17 +3,24 @@ from django.contrib import admin
 from .models import Article, Entry
 
 
-class ArticleAdmin(admin.ModelAdmin):
-    search_fields = ['title', ]
-    list_filter = ['created_at', ]
-    list_display = ['title', 'user', 'created_at', ]
-
-
-class EntryAdmin(admin.ModelAdmin):
+class EntryInline(admin.StackedInline):
+    model = Entry
     search_fields = ['restaurant_name', ]
     list_filter = ['created_at', ]
     list_display = ['restaurant_name', 'article', ]
 
+    def get_max_num(self, request, obj=None, **kwargs):
+        max_num = 12
+        return max_num
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title', ]
+    list_filter = ['created_at', ]
+    list_display = ['title', 'user', 'created_at', ]
+    inlines = [
+        EntryInline,
+    ]
+
 
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(Entry, EntryAdmin)
--- articles/models.py
+++ articles/models.py
@@ -17,10 +17,10 @@ class Article(models.Model):
 class Entry(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='articles/photos/entryphotos/', blank=True, null=True)
     restaurant_name = models.CharField(max_length=255)
     content = models.TextField()
     restaurant_url = models.CharField(max_length=255, blank=True, null=True)
-    photo = models.ImageField(upload_to='articles/photos/entryphotos/', blank=True, null=True)
 
     def __str__(self):
         return self.restaurant_name
